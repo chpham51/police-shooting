f9916029376975affa2c48d19aab420bfbf95fc3
chpham51 <phamchristy95@gmail.com>
2015-07-07 16:10:06 -0700

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1,19 +1,14 @@
 // Function to draw your map
 var drawMap = function() { 
-	// Create map and set view
 	var map = L.map("container");
 	map.setView([38, -100], 4);
-	// Create an tile layer variable using the appropriate url
 	var layer = L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png");
-	// Add the layer to your map
 	layer.addTo(map);
-	// Execute your function to get data
 	getData(map);
 }
 
 // Function for getting data
 var getData = function(map) {
-	// Execute an AJAX request to get the data in data/response.js
 	var data;
 	$.ajax({
 	  	url: "data/response.json", 
@@ -21,7 +16,6 @@ var getData = function(map) {
 	  	success:function(dat) {
 	  		data = dat;
 	  		customBuild(map, data);
-	  		console.log("Success!!!")
 	  	},
 		dataType:"json"
 	})
@@ -29,15 +23,55 @@ var getData = function(map) {
 
 // Do something creative with the data here!  
 var customBuild = function(map, data) {
+	var females = [];
+	var males = [];
+	var unknownGender = [];
 	data.map(function(d) {
-		var marker = new L.circleMarker([d.lat, d.lng], 20, 
-					{color: "blue", opacity: .5});
+		var markerOpacity; 
+		var markerColor; 
+
+		if (d["Armed or Unarmed?"] == "Armed") {
+			markerOpacity = 0;
+		} else if (d["Armed or Unarmed?"] == "Unarmed") {
+			markerOpacity = 1; 
+		} else {
+			markerOpacity = 0.5;
+		}
+
+		if (d["Race"] == "White") { 
+			markerColor = "blue";
+		} else if (d["Race"] == "Black or African American") { 
+			markerColor = "purple";
+		} else if (d["Race"] == "Asian") { 
+			markerColor = "green";
+		} else if (d["Hispanic or Latino Origin"] == "Hispanic or Latino Origin") { 
+			markerColor = "black";
+		} else {
+			markerColor = "gray";
+		}
+
+		var marker = new L.circleMarker([d.lat, d.lng], {color: markerColor, opacity: markerOpacity, radius: 3});
+		marker.bindPopup(d["Victim Name"])
 		marker.addTo(map);
 
-	});
+		if (d["Victim's Gender"] == "Female") {
+			females.push(marker);
+		} else if (d["Victim's Gender"] == "Male") {
+			males.push(marker);
+		} else {
+			unknownGender.push(marker); 
+		}
+	})
+	var baseMaps;
+
+	var femalesLayer = L.layerGroup(females);
+	var malesLayer = L.layerGroup(males);
+	var unknownGenderLayer = L.layerGroup(unknownGender);
+
+	var overlayMaps = {
+    	"Female": femalesLayer,
+    	"Male": malesLayer,
+    	"Unknown Gender": unknownGenderLayer
+	}
+	L.control.layers(baseMaps, overlayMaps).addTo(map);
 }
-
-
-
-
-
